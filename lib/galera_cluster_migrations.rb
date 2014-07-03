2c20a93b42b15c5e734f90be6b428befd9adb46a
Bryan Traywick <bryan@railsmachine.com>
2014-07-03 14:40:20 -0400

    Initial commit.

---
+++ lib/galera_cluster_migrations.rb
@@ -0,0 +1,5 @@
+require "galera_cluster_migrations/version"
+
+module GaleraClusterMigrations
+  # Your code goes here...
+end
---
+++ lib/galera_cluster_migrations/version.rb
@@ -0,0 +1,3 @@
+module GaleraClusterMigrations
+  VERSION = "0.0.1"
+end
