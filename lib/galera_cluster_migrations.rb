a8f1d70fbaf7ca1a31615c42ae37ca12842d9dd5
Bryan Traywick <bryantraywick@gmail.com>
2014-11-22 14:08:18 -0500

    Disable wsrep_on so that the schema_migrations update won't be replicated.

--- lib/galera_cluster_migrations.rb
+++ lib/galera_cluster_migrations.rb
@@ -17,6 +17,7 @@ module GaleraClusterMigrations
     def with_rsu
       enable_rsu
       yield
+      disable_wsrep_on
     ensure
       enable_toi
     end
@@ -25,6 +26,13 @@ module GaleraClusterMigrations
       say "Setting wsrep_OSU_method to TOI"
       unless [:development, :test].include?(Rails.env)
         execute "SET GLOBAL wsrep_OSU_method=TOI"
+      end
+    end
+
+    def disable_wsrep_on
+      say "Setting wsrep_on to OFF"
+      unless [:development, :test].include?(Rails.env)
+        execute "SET wsrep_on=OFF"
       end
     end
   end
--- lib/galera_cluster_migrations/version.rb
+++ lib/galera_cluster_migrations/version.rb
@@ -1,3 +1,3 @@
 module GaleraClusterMigrations
-  VERSION = "1.0.1.beta1"
+  VERSION = "1.0.1.beta2"
 end
